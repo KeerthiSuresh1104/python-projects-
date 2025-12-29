e6d98d75a3217ebc915e670b4ca655c23c4bedb8
Keerthi <keerthisuresh1104@gmail.com>
2025-12-29 22:29:44 +0530

    day1

---
+++ day1/exercise1.py
@@ -0,0 +1,4 @@
+print("welcome to the Band name generator")
+city = input("which city did you grow up in ?\n ")
+pet = input("what is your  pet name? \n")
+print("your band name could be:\n " + city + " " + pet)
