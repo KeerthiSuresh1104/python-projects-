b3329160e2c6512114c0659269e56ff12726d2d4
KeerthiSuresh1104 <keerthisuresh1104@gmail.com>
2026-01-02 23:25:15 +0530

    day5

---
+++ day5/password_generator.py
@@ -0,0 +1,19 @@
+import random
+letters =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','V','W','X','Y','Z']
+numbers=['0','1','2','3','4','5','6','7','8','9']
+symbols=['!','@','#','$','%','&','*','?']
+
+print("welcome to password generator")
+
+nr_letters=int(input("how many letters do you want?\n"))
+nr_symbols=int(input("how many symbols do you want?\n"))
+nr_numbers=int(input("how many numbers do you want?\n"))
+
+password = ""
+for i in range(1 ,nr_letters+1):
+    password += random.choice(letters)
+for i in range(1,nr_symbols+1):
+    password += random.choice(symbols)
+    for i in range(1,nr_numbers+1):
+        password += random.choice(numbers)
+print(password)
